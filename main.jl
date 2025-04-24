0498329aa5f66fe3def30b91d5429d70d1d41b04
= <aryan.r.jha@gmail.com>
2025-04-24 05:50:11 -0700

    simulation 1) slow 2) wrong

--- main.jl
+++ main.jl
@@ -3,21 +3,18 @@ include("./src/setupIntCombOpt567.jl")
 Revise.track(IntCombOpt567.setCoveringHeuristics)
 Revise.track(IntCombOpt567.helperFunctions)
 
-testCase = "p4m6"
+# testCase = "p4m6"
 # testCase = "phase1"
-# testCase = "cap360"
+testCase = "cap360"
 
-graph = sCH.initializeGraph("rawData/project02/" * testCase * ".txt")
+sim_time = @belapsed begin
+    g = sCH.initializeGraph("rawData/project02/" * testCase * ".txt")
+    sCH.solveSetCoveringProblem!(g)
+    global graph = g
+end
 
-graph = sCH.solveSetCoveringProblem(graph)
-
-@unpack poles_used, meters_covered, Acov, m = graph;
+@unpack poles_used, meters_covered, Acov, m = graph
 @test meters_covered == m
-myprintln(true, "Before value=$(poles_used)")
-display(Acov)
+myprintln(true, "value=$(poles_used), time=$(sim_time)")
 
-sCH.removePole!(graph, 2, verbose=true)
-@unpack meters_covered, poles_used, Acov, m = graph;
-@test meters_covered == m 
-myprintln(true, "After value=$(poles_used)")
-display(Acov)
+# display(Acov)
