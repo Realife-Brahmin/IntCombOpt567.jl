94f38ed703623deb23062796719f257d316f01df
= <aryan.r.jha@gmail.com>
2025-04-27 14:29:47 -0700

    Refactor code structure for improved readability and maintainability

--- main.jl
+++ main.jl
@@ -4,10 +4,10 @@ Revise.track(IntCombOpt567.setCoveringHeuristics)
 Revise.track(IntCombOpt567.helperFunctions)
 
 # testCase = "p4m6"
-# testCase = "phase1"
+testCase = "phase1"
 testCase = "cap360"
 cleanupRepeats = 1
-# cleanupRepeats = 10
+cleanupRepeats = 10
 scoring_function = "greedy"
 # scoring_function = "score1"
 # scoring_function = "score2"
@@ -20,19 +20,22 @@ g = sCH.initializeGraph("rawData/project02/" * testCase * ".txt",
 
 #region solveSetCoveringProblem
 if benchmarkTime
+    # just time the solve call
     sim_time = @belapsed begin
-        @profile sCH.solveSetCoveringProblem!(g)
-        global graph = g
+        g_local = deepcopy(g)
+        sCH.solveSetCoveringProblem!(g_local)
     end
+    println("Solved in $(sim_time) s")
 else
-    @profile begin
-        global graph = sCH.solveSetCoveringProblem!(g)
-    end
     sim_time = "not_benchmarked"
-end 
-#endregion solveSetCoveringProblem
+end
 
-myprintln(true, "sim_time=$(sim_time)")
+# profile separately (only once)
+@profile begin
+    sCH.solveSetCoveringProblem!(g)
+    global graph = g
+end
+#endregion solveSetCoveringProblem
 
 @unpack poles_used, meters_covered, Acov, m = graph
 @test meters_covered == m
@@ -40,7 +43,6 @@ myprintln(true, "value=$(poles_used)")
 
 # display(Acov)
 
-# Save the top 20 lines (most sampled = most time spent) to a file
 open("profile_summary.txt", "w") do io
     Profile.print(io; format=:flat, sortedby=:count)
 end
